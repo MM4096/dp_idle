e0d12b8a813ae22481c79d3fe5713fe65fa7209e
enoch wu <enoch.wu@kingsway.school.nz>
2021-10-19 11:43:27 +1300

    Commit

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
-var xp;
 var money;
 var level;
+var totalxp;
 let maxBuildingCount = [0, 0, 0, 0, 0, 0, 0, 0];
-const xpToLevelUp = [-5, -1, 15, 99, 255, 999, 1499, 2199];
-function level() {
-    for (var i = 1; i < length.xpToLevelUp + 1; i++) {
-        if (xp > xpToLevelUp[i]) {
-            level = i;
-        } else {
+const xpToLevelUp = [-5, -1, 15, 99, 255, 999, 1499, 2199, 9999];
+const itemCosts = ["undefined", 10];
+const itemXP = ["undefined", 10];
+function level(xp) {
+    for (var i = 1; i < 99; i++) {
+        if (parseInt(xp) < xpToLevelUp[i] + 1) {
             return (i - 1);
         }
     }
+}
+function returnLevel() {
+    var a = level("2200");
+    document.getElementById("xpLevel").innerHTML = a;
+}
+function buyItem(itemNumber) {
+    changeXP = itemXP[itemNumber]
+    changeMoney = itemCosts[itemNumber]
+    money -= changeMoney;
+    totalxp += changeXP;
+    update();
+}
+function update() {
+    document.getElementById("money").innerHTML = money;
+    document.getElementById("xp").innerHTML = totalxp;
+    level = level(totalxp);
+    document.getElementById("level").innerHTML = level;
 }
